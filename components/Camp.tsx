0e2722423596ed4c1855501c824743fcf238c15f
ZakariaBatty <zbatty1297@gmail.com>
2024-05-08 08:12:31 +0100

    add hero an com section

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,6 +1,44 @@
+import Image from "next/image"
+
+interface CampProps {
+    backgroundImage: string,
+    title: string,
+    subtitle: string,
+    peaopleJoined: string
+}
+
+const CamSite = ({ backgroundImage, title, subtitle, peaopleJoined }: CampProps) => {
+    return (
+        <div className={`h-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
+            <div className="flex h-full flex-col items-start justfiy-between p-6 lg:px-20 lg:py-10">
+                <div className="flexCenter gap-4">
+                    <div className="rounded-full bg-green-50 p-4">
+                        <Image src="/folded-map.svg" alt="map" width={28} height={28} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 const Camp = () => {
     return (
-        <div>Camp</div>
+        <section className="border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:md10 lg:py-20 xl:mb-20">
+            <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+                <CamSite
+                    backgroundImage="bg-bg-img-1"
+                    title="Putuk Truno Camp"
+                    subtitle="Prigen, Pasuruan"
+                    peaopleJoined="50+ Joined"
+                />
+                <CamSite
+                    backgroundImage="bg-bg-img-1"
+                    title="Putuk Truno Camp"
+                    subtitle="Prigen, Pasuruan"
+                    peaopleJoined="50+ Joined"
+                />
+            </div>
+        </section>
     )
 }
 
